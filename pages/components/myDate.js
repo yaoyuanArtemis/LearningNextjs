bded7e10251fc8ae31c7c3511b9b439bd3b2ce29
yaoyuanArtemis <liufeng0194@gmail.com>
2025-05-09 14:24:45 +0800

    add:增加主页跳转&增加环境变量

--- pages/components/myDate.js
+++ pages/components/myDate.js
@@ -2,8 +2,16 @@ import { useState, useEffect } from "react";
 
 export default function MyDate() {
   const [dt, setDT] = useState();
+  const date = new Date();
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const hours = String(date.getHours()).padStart(2, "0");
+  const minutes = String(date.getMinutes()).padStart(2, "0");
+  const seconds = String(date.getSeconds()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   useEffect(() => {
-    setDT(new Date().toString());
+    setDT(formattedDate);
   }, []);
-  return <h4>{dt}</h4>;
+  return <h4 style={{ display: "flex", justifyContent: "center" }}>{dt}</h4>;
 }
--- pages/pets/index.js
+++ pages/pets/index.js
@@ -1,11 +1,43 @@
+import { useRouter } from "next/router";
 import MyDate from "../components/myDate";
 import RouterButton from "../components/RouterButton";
-export default function Pets() {
+import Link from "next/link";
+export default function Pets({ data }) {
+  const currentDomain = process.env.NEXT_PUBLIC_DOMAIN;
+  const router = useRouter();
+  const asPath = router.asPath;
+  console.log("asPath", asPath);
+  console.log("currentDomain", currentDomain);
+  const realData = data?.results || [];
+  const counts = data.count;
   return (
     <>
       <h1>Pets</h1>
       <MyDate />
       <RouterButton />
+      <h5>Pockmon Total Count:{counts}</h5>
+      <div>
+        <ul>
+          {realData.map((item) => {
+            return (
+              <li>
+                <Link href={currentDomain + asPath + "/" + item.name}>
+                  {item.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon`).then((res) =>
+    res.json()
+  );
+  return {
+    props: { data: res },
+  };
 }
