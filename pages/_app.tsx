1f070ccd860b00869ce177f7148cfe519c2ef0d1
yaoyuanArtemis <liufeng0194@gmail.com>
2025-05-10 13:32:31 +0800

    add:添加Vercel-SpeedInsight工具

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <SpeedInsights />
+      <Component {...pageProps} />
+    </>
+  );
 }
